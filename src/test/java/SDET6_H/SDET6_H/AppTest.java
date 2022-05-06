41206b628e57fa85461429d5009498121bc10f2e
Sindh <Sindh@DESKTOP-5CMT1P5>
2022-05-06 10:43:54 +0530

    commit1

---
+++ src/test/java/SDET6_H/SDET6_H/AppTest.java
@@ -0,0 +1,20 @@
+package SDET6_H.SDET6_H;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
